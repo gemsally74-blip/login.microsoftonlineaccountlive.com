49f5f840015749e9d36971709edf0e1eb37cecc6
gemsally74-blip <gemsally74@gmail.com>
2026-09-04 18:33:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ if (empty($_SESSION['verified'])) {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sign In to your account</title>
   <link rel="icon" href="favicon.png">
   <link rel="preload"
