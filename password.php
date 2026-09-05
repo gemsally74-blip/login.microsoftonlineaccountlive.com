f63206ecbbd297f8c84638a31001ce4027472413
gemsally74-blip <gemsally74@gmail.com>
2026-09-04 18:35:46 -0700

    Update password.php

--- password.php
+++ password.php
@@ -7,6 +7,7 @@ $email = $_SESSION["demo_email"] ?? "";
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sign In to your account</title>
   <link rel="icon" href="favicon.png">
   <link rel="preload"
